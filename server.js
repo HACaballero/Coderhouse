7565380229e629732ebd6862a9d6bca68135c60d
Alexis <acaballero.dev@gmail.com>
2022-04-09 01:01:58 -0300

    Clase 12- Sessions

--- server.js
+++ server.js
@@ -20,6 +20,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
+//Session
+var session = require("express-session");
+const MongoStore = require("connect-mongo");
+const advancedOptions = {useNewUrlParser:true, useUnifiedTopology:true}
+app.use(
+	session({
+		store: MongoStore.create({ mongoUrl: "mongodb+srv://ACaballero:afotNbT5cDOvGazW@cluster0.rkbgt.mongodb.net/sesiones?retryWrites=true&w=majority",mongoOptions:advancedOptions }),
+		secret: "secretDB",
+		resave: false,
+		saveUninitialized: false,
+
+		cookie: {
+			expires: 600000,
+		},
+	})
+);
 const { connect } = require("./mongoDB/connect");
 connect();
 app.engine(
@@ -36,15 +52,50 @@ createProductosTable();
 createMensajesTable();
 
 app.get("/", async (req, res) => {
+	if (!req.session.username) {
+		res.redirect("/login");
+	}
+	console.log(`Visitó la pag ${req.session.contador} veces.`);
 	productos = await selectProductos();
 	productos = JSON.parse(JSON.stringify(productos));
 	let { mensajes, comprension } = await getMensajes();
 	res.render("main", {
 		layout: "index",
 		list: productos,
+		session: req.session.username ? true : false,
+		user: req.session.username,
 		mensajes: mensajes,
 		comprension: comprension,
 		empty: productos.length == 0 ? true : false,
+	});
+});
+
+app.get("/login", async (req, res) => {
+	if (req.session.username) {
+		res.redirect("/");
+	}
+	productos = await selectProductos();
+	productos = JSON.parse(JSON.stringify(productos));
+	let { mensajes, comprension } = await getMensajes();
+	res.render("login", {
+		layout: "index",
+		list: productos,
+		mensajes: mensajes,
+		comprension: comprension,
+		empty: productos.length == 0 ? true : false,
+	});
+});
+
+app.get("/logout", async (req, res) => {
+	let user = req.session.username;
+	req.session.destroy((err) => {
+		if (err) {
+			return res.json({ status: "Logout ERROR", body: err });
+		}
+	});
+	res.render("logout", {
+		layout: "index",
+		user,
 	});
 });
 
@@ -73,6 +124,14 @@ app.get("/productos-test", async (req, res) => {
 		empty: productos.length == 0 ? true : false,
 	});
 });
+
+app.post("/login", async (req, res) => {
+	if (req.body.username) {
+		req.session.username = req.body.username;
+	}
+	res.redirect("/");
+});
+
 httpServer.listen(8080, () => console.log("SERVER ON"));
 
 io.on("connection", (socket) => {
